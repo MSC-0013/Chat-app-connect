0835afc234b8768df80c8cba559156268dcc332c
MSC-0013 <soumyashreerout99@gmail.com>
2025-09-29 15:08:40 +0530

    mm

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 
-const storage = multer.memoryStorage(); // temporary memory storage
+const storage = multer.memoryStorage(); 
 const upload = multer({ storage });
 
 module.exports = upload;
